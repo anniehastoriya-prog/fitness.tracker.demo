c6dcda01530ada8ce3cd2915a48c098a9a886965
Annie <anniehastoriya@gmail.com>
2026-05-22 19:46:58 -0500

    submit

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,9 +1,43 @@
-export default function ActivityList({ activities }) {
+import { useState } from "react";
+import { deleteActivity } from "../api/activities";
+import { useAuth } from "../auth/AuthContext";
+// renders a list of activites
+export default function ActivityList({ activities, syncActivities }) {
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
+        <ActivityListItem
+          key={activity.id}
+          activity={activity}
+          syncActivities={syncActivities}
+        />
       ))}
     </ul>
+  );
+}
+// renders single activity with a delete button
+function ActivityListItem({ activity, syncActivities }) {
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+
+  const tryDelete = async () => {
+    setError(null);
+
+    try {
+      await deleteActivity(token, activity.id);
+      syncActivities();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  // the delete button only shows if the user is logged in
+  // and the error error message only show is there is an error
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && <button onClick={tryDelete}>Delete</button>}
+      {error && <p role="alert"> {error}</p>}
+    </li>
   );
 }
--- src/api/activities.js
+++ src/api/activities.js
@@ -35,3 +35,18 @@ export async function createActivity(token, activity) {
     throw Error(result.message);
   }
 }
+//this sends a request ot the API to delete the actvity with the givem login ID.
+// the correct token is requied and will throw an error message if the user is not signed in.
+export async function deleteActivity(token, id) {
+  if (!token) {
+    throw Error(" You must be signed in to delete an activity");
+  }
+  const response = await fetch(API + "/activites/" + id, {
+    method: "DELETE",
+    heder: { Authorizatio: "Bearer" + token },
+  });
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.meesage);
+  }
+}
